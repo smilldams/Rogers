ea7e6c28fd2ef5403211202a4c9a6c443433be91
smilldams <147149172+smilldams@users.noreply.github.com>
2023-10-06 10:50:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_5is2juu", "template_vy11ntf", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
